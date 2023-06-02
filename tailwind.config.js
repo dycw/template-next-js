baf8f01bff5a1716bf0780d9064e288f209289da
Derek Wan <d.wan@icloud.com>
2023-06-02 08:25:23 +0800

    Properly Bootstrap the app (#5)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    // For the best performance and to avoid false positives,
-    // be as specific as possible with your content configuration.
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
 };
