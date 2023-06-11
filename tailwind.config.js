50432d30ab3e468f40d56e9b4132c6e5c340ab1a
Derek Wan <d.wan@icloud.com>
2023-06-11 09:09:39 +0800

    Remove `container-queries` (#17)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+export default {
+  content: ["./src/**/*.tsx"],
   corePlugins: {
     aspectRatio: false,
   },
   plugins: [
     require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/container-queries"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
   ],
